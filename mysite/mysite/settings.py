f5504e4cd5b87a4c4ee11e5a63f240d9c54abc75
sandalkumar <sanadldotin@gmail.com>
2019-04-27 14:43:45 +0530

    change Static config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a14kv%e)n^^u0t0t#65jebb2yuw#b(&qrocvii9@_2*ppt!i=*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
